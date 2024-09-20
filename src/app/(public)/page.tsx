70b7922c9e90020b645376fd5381ef48116bc705
Anderson Fariña <inumansoul@gmail.com>
2024-09-19 20:26:27 -0400

    Home page design update

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
 
       <section className="py-28 bg-neutral-100">
         <div className="max-w-6xl mx-auto">
-        <h2 className="text-3xl font-bold">¿Por qué elegir Printit?</h2>
+        <h2 className="text-3xl font-bold mb-8 text-center">¿Por qué elegir Printit?</h2>
         <ul className="grid grid-cols-6 grid-rows-6 gap-4">
           <li className="border border-neutral-300 flex flex-col justify-end rounded-xl p-5 bg-white col-span-3 row-span-4">
             <p className="text-xl font-bold">Facturación simplificada</p>
